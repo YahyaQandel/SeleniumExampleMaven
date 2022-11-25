0a7977f0885a6c4d175259c3dc114b767996009e
YahyaQandel <yahya.qandel@gmail.com>
2022-11-25 22:05:59 +0200

    update mystore credentials

--- src/test/java/tests/TestMyStore.java
+++ src/test/java/tests/TestMyStore.java
@@ -3,7 +3,6 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -11,21 +10,18 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.io.IOException;
 import java.time.Duration;
-import java.util.List;
 
-import static org.junit.Assert.*;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TestMyStore {
 
-    private static final String MYSTORE_URL = "http://automationpractice.com/index.php?controller=authentication&back=my-account";
+    private static final String MYSTORE_URL = "https://djangogreatkart.com/accounts/login/";
     private WebDriver driver;
     private String accountEmail = "yahya.qandel@gmail.com";
     private String accountPassword = "VDdReTVFIWE0UCEhCg";
     @Before
-    public void prepare() throws  {
+    public void prepare()  {
         WebDriverManager.chromedriver().setup();
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--headless");
